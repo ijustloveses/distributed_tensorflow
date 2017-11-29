f2ee59ff76c4eb53b878871412c1715fd4be2c20
tkamc <tkamc@DualGTX1080.tkamc.com>
2017-11-29 16:59:47 +0800

    add settings

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # encoding: utf-8
 
-ps_svrs = ['10.88.104.31:2224']
-worker_svrs = ['10.88.104.31:2222', '10.88.104.31:2223', '10.88.102.119:2222']
+ps_svrs = ['server1:2224']
+worker_svrs = ['server1:2222', 'server1:2223', 'server2:2222']
